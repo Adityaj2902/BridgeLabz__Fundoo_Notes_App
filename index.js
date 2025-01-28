c6acb7ec396d75ba2ba5d0aaecd152105f63a59c
Adityaj2902 <adityajofficial909@gmail.com>
2025-01-28 20:14:36 +0530

    Basic Setting Has been Done

---
+++ index.js
@@ -0,0 +1,36 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+// const authRoutes = require('./routes/authRoutes');
+// const errorHandler = require('./middleware/errorHandler');
+
+// Initialize dotenv for environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+app.use(cors());
+
+// Routes
+// app.use('/api/auth', authRoutes);
+
+// Global error handling middleware
+// app.use(errorHandler);
+
+// Connect to MongoDB
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch(err => console.log("Failed to connect to MongoDB:", err));
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
