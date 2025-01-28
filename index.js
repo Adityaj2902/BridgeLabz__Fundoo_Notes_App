02b70f502b25abe3701df11b8891b233642830ad
Adityaj2902 <adityajofficial909@gmail.com>
2025-01-28 23:00:24 +0530

    Done WIth  Structure Part From the Scractch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
+
 
 // Initialize dotenv for environment variables
 dotenv.config();
@@ -14,18 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes
-app.use('/api/auth', authRoutes);
 
 
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.log("Failed to connect to MongoDB:", err));
 
 // Start server
 const PORT = process.env.PORT || 3000;
