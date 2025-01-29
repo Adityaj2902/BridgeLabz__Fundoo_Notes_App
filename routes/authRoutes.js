ee0c5bdc04605ce96989c720e2a13ca840e65874
Adityaj2902 <adityajofficial909@gmail.com>
2025-01-29 09:37:19 +0530

    Essential Thing Only

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { registerUser } = require('../controllers/registerController');
+const { loginUser } = require('../controllers/loginController');
+
 
 const router = express.Router();
 
+// Register User
 
 router.post('/register', registerUser);
 
+// Login route
+
+router.post('/login', loginUser);
 
 
 module.exports = router;
