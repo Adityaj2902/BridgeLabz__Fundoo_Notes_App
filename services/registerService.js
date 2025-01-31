0819f1fcfdfa0744061007ca37561122e86943bb
Adityaj2902 <adityajofficial909@gmail.com>
2025-01-31 11:02:59 +0530

    [Aditya Jaiswal Operation:Add Implemenet the feature of bycrypt

--- services/registerService.js
+++ services/registerService.js
@@ -1,20 +1,48 @@
+// const User = require('../models/user');
+
+// const registerUser = async (firstName, lastName, email, password) => {
+//   const existingUser = await User.findOne({ email });
+//   if (existingUser) {
+//     throw new Error("Email already in use");
+//   }
+
+//   const newUser = new User({
+//     firstName,
+//     lastName,
+//     email,
+//     password, 
+//   });
+
+//   await newUser.save();
+//   return newUser; 
+// };
+
+// module.exports = {
+//   registerUser,
+// };
+const bcrypt = require('bcryptjs'); 
 const User = require('../models/user');
-
 const registerUser = async (firstName, lastName, email, password) => {
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    throw new Error("Email already in use");
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new Error('Email already in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10); 
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword, 
+    });
+
+    await newUser.save();
+    return newUser; 
+  } catch (error) {
+    throw new Error(error.message); 
   }
-
-  const newUser = new User({
-    firstName,
-    lastName,
-    email,
-    password, 
-  });
-
-  await newUser.save();
-  return newUser; 
 };
 
 module.exports = {
